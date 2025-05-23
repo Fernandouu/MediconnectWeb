c6f349f406a4203fa13105e2e91a9cf774795abc
StannDubon <stalin.dbn@gmail.com>
2025-05-23 12:56:42 -0600

    login y registro terminado

--- tests/controllers/dashboard.js
+++ tests/controllers/dashboard.js
@@ -1,9 +1,18 @@
 (async () => {
+
+    if(!localStorage.getItem('authToken')){
+        window.location.href = "index.html";
+    }
+
     const response = await fetchData('user', 'GET');
     document.getElementById("name").innerHTML = `Bienvenido ${response.nombre} ${response.apellido}`;
     document.getElementById("info").innerHTML = `Se encuentra en modo ${response.type}`;
-    
-    if(response.type=="patient"){
-        
-    }
 })();
+
+document.getElementById("logout").addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    localStorage.clear();
+    alert("Sesión Cerrada");
+    location.reload();
+});
